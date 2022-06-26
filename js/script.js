da724ce9865b4ee390959e9a5986fb438433b0fd
antaninazhuk <101988172+antaninazhuk@users.noreply.github.com>
2022-06-26 17:49:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Task 1
+
+// let user = {
+//     userName: 'Tonya',
+//     programLanguage: 'JS',
+//     numberOfGroup: 32
+// }
+
+// class Developer {
+//     constructor(param) {
+//         this.name = param.userName,
+//             this.programLanguage = param.programLanguage
+//     }
+//     say() {
+//         console.log(this.name, 'работает с',
+//             this.programLanguage);
+//     }
+// }
+
+// const myUser = new Developer(user);
+
+// console.log(myUser);
+
+// class Student extends Developer {
+//     constructor(param) {
+//         super(param);
+
+//         this.numberOfGroup = param.numberOfGroup;
+//     }
+//     say() {
+//         super.say()
+//     }
+// }
+
+// const myUser2 = new Student(user);
+// console.log(myUser2);
+
+// Task 2
+
+// String.prototype.addExclMark = function() {
+//     return this + '!'
+// }
+
+// let str = 'Tonya';
+// console.log(str.addExclMark());
+
+// Task 3
+
+// Number.prototype.dividByTwo = function() {
+//     return this / 2;
+// }
+
+// let num = 132;
+// console.log(num.dividByTwo());
